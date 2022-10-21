401ab8ed6893d0bbae6a1fc9f57b3c8fb992fbd6
kirthin12 <krajkumar@ucsd.edu>
2022-10-21 10:18:18 -0700

    edited

--- DocSearchTest.java
+++ DocSearchTest.java
@@ -1,4 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.net.URI;
+
+
 public class DocSearchTest 
 {
+    @Test
+    public void testHandleRequest()
+    {
+        Handler test = new Handler("technical\\biomed\\");
+        URI url = new URI("http://localhost:4000");
+        assertEquals("Don't know how to handle that path!" , test.handleRequest(url));
+    }
     
 }
